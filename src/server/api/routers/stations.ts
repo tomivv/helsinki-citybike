e7ebafc8b1b47ba9ce5e294180fc12936b2aa839
Tomi Niiranen <tominiiranen@hotmail.com>
2023-01-19 13:45:56 +0200

    Added stationsRouter to trpc

---
+++ src/server/api/routers/stations.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const stationsRouter = createTRPCRouter({
+  getStations: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.departure_station.findMany();
+  })
+});
