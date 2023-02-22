b01c184ea7a4d80134deb145456bc6322c5b5e2e
Tomi Niiranen <tominiiranen@hotmail.com>
2023-02-22 12:42:09 +0200

    Fix: tests looked at wrong spans

--- tests/journeys.spec.ts
+++ tests/journeys.spec.ts
@@ -20,13 +20,13 @@ test('pagination next and prev buttons work', async ({ page }) => {
   await page.locator('Button:text("Next")').click();
 
   // check that second span has text 11 and third span has text 20
-  await expect(page.locator(':nth-match(span, 2)')).toHaveText("11");
-  await expect(page.locator(':nth-match(span, 3)')).toHaveText("20");
+  await expect(page.locator(':nth-match(span, 3)')).toHaveText("11");
+  await expect(page.locator(':nth-match(span, 4)')).toHaveText("20");
 
   // Click prev button
   await page.locator('Button:text("Prev")').click();
 
   // check that spans have gone back to 1 to 10
-  await expect(page.locator(':nth-match(span, 2)')).toHaveText("1");
-  await expect(page.locator(':nth-match(span, 3)')).toHaveText("10");
+  await expect(page.locator(':nth-match(span, 3)')).toHaveText("1");
+  await expect(page.locator(':nth-match(span, 4)')).toHaveText("10");
 })
